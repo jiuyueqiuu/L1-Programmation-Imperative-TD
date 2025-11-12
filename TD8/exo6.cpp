7c184b93ee3712d8051f9576d371d406d3694157
Haoran Yin <haoran.hry@gmail.com>
2025-11-12 15:23:07 +0100

    Ajouté l'exercice #6

---
+++ TD8/exo6.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+/** copie le contenu d'un fichier texte dans un autre
+ *  @param source1 le nom du fichier source
+ *  @param source2 le nom du fichier source
+ *  @param source3 le nom du fichier source
+ *  @param destination le nom du fichier destination
+ **/
